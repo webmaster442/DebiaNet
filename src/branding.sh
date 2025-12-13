1b6246721bd7763b793c2801a56cee399ceb4952
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-12-13 17:35:27 +0100

    Edited OS Release info

--- src/branding.sh
+++ src/branding.sh
@@ -6,6 +6,9 @@ cp ./branding/wsl-distribution.conf /etc/wsl-distribution.conf
 rm /usr/lib/wsl/debian_logo.ico
 cp ./branding/debianet.ico /usr/lib/wsl/debianet.ico
 
+rm /etc/os-release
+cp ./branding/os-release.conf /etc/os-release
+
 rm /usr/lib/wsl/oobe.sh
 cp ./branding/oobe.sh /usr/lib/wsl/oobe.sh
 
@@ -15,3 +18,4 @@ chmod 755 /etc/wsl-distribution.conf
 chmod 755 /usr/lib/wsl/oobe.sh
 chmod 755 /usr/lib/wsl/debianet.ico
 chmod 755 /usr/lib/wsl/terminal-debianet.json
+chmod 755 /etc/os-release
